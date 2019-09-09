759a8536144e4b9d398e25b19ca79d1272171744
bazyrch <bazyrch@gmail.com>
2019-09-10 02:55:13 +0300

    1. Задать температуру в градусах по Цельсию. Вывести в alert соответствующую температуру в градусах по Фаренгейту. Подсказка: расчёт идёт по формуле: Tf = (9 / 5) * Tc + 32, где Tf – температура по Фаренгейту, Tc – температура по Цельсию 2. Работа с переменными. a) Объявить две переменные: admin и name. Записать в name строку «Василий»; b) Скопировать значение из name в admin. Вывести admin (должно вывести «Василий»). 4. *Чему будет равно JS-выражение 1000 + "108"?

---
+++ script.js
@@ -0,0 +1,12 @@
+var a = +prompt('Введите температуру по Цельсию');
+var c = (9 / 5) * a + 32;
+console.log('Температура по Фаренгейту равна: ' + c);
+
+let admin;
+let name;
+name = "Василий";
+admin = name;
+console.log(admin);
+
+console.log(1000 + "108");
+//Ответ: 1000108
